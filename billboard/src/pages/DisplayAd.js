e3cb7b4a23429e7f3ceb5c75d259e82d4accae1d
SanjuLA-01 <61306450+SanjuLA-01@users.noreply.github.com>
2021-03-30 22:44:15 +0530

    Create DisplayAd.js

---
+++ billboard/src/pages/DisplayAd.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class DisplayAd extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello</h1>
+            </div>
+
+        )
+    }
+}
+export default DisplayAd;
