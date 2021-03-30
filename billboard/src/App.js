59f3cf5874f20b951956a030bdd7d8d5e593ddb3
SanjuLA-01 <61306450+SanjuLA-01@users.noreply.github.com>
2021-03-30 22:44:11 +0530

    Update App.js

--- billboard/src/App.js
+++ billboard/src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router,Route,Switch, Link} from 'react-router-dom';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from 'axios';
+import DisplayAd from './pages/DisplayAd';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light float-right">
         <Link to="/" className="nav-link">Dashboard</Link>
         <Link to="/advertisements" className="nav-link">Advertisements</Link>
+        <Link to= "/display" className="nav-link">DisplayAd</Link>
       </nav>
       <Switch>
         <Route exact path='/' component={Main} /> // was changed from home to main
