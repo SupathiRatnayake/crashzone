ff956b607e5db77af4834ded30f3bd05657ef186
SanjuLA-01 <61306450+SanjuLA-01@users.noreply.github.com>
2021-03-31 00:01:08 +0530

    Update DisplayAd.js

--- billboard/src/pages/DisplayAd.js
+++ billboard/src/pages/DisplayAd.js
@@ -1,6 +1,66 @@
 import React,{Component} from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
 
 class DisplayAd extends React.Component{
+
+      slideImages =  [
+        {
+          "name" : "Hits On Fire",
+          "targetProfessionals": false,
+          "imageURL": "",
+        },
+        {
+          "name" : "So Far So Good",
+          "targetProfessionals": true,
+          "imageURL": "",
+        },
+        {
+          "name" : "The Best of Me",
+          "targetProfessionals": false,
+          "imageURL": "",
+        },
+        {
+          "name" : "Run to You",
+          "targetProfessionals": true,
+          "imageURL": "",
+        },
+        {
+          "name" : "Summer of '69",
+          "targetProfessionals": true,
+          "imageURL": "",
+        },
+        {
+          "name" : "Victim of Love",
+          "targetProfessionals": false,
+          "imageURL": "",
+        }
+      ];
+
+       Slideshow = () => {
+        return (
+          <div className="slide-container">
+            <Slide>
+              <div className="each-slide">
+                <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+                  <span>Slide 1</span>
+                </div>
+              </div>
+              <div className="each-slide">
+                <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+                  <span>Slide 2</span>
+                </div>
+              </div>
+              <div className="each-slide">
+                <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+                  <span>Slide 3</span>
+                </div>
+              </div>
+            </Slide>
+          </div>
+        )
+    }
+
     render(){
         return(
             <div>
